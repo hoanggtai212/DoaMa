30f64607a978f74044bffcf844d482a4992405b1
HT <rongchube99@gmail.com>
2026-05-24 20:44:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,21 +5,12 @@ const candleImage = document.getElementById("candleImage");
 // tạo text đếm ngược
 const countdownText = document.createElement("div");
 
-countdownText.style.position = "fixed";
-countdownText.style.top = "50%";
-countdownText.style.left = "50%";
-countdownText.style.transform = "translate(-50%, -50%)";
-countdownText.style.fontSize = "120px";
-countdownText.style.color = "red";
-countdownText.style.fontWeight = "bold";
-countdownText.style.zIndex = "9999";
-countdownText.style.display = "none";
+countdownText.classList.add("countdown");
 
 document.body.appendChild(countdownText);
 
 function blowCandle() {
 
-  // đổi ảnh nến
   candleImage.src = "./style/Candle-Off.png";
 
   let count = 3;
@@ -32,20 +23,19 @@ function blowCandle() {
     count--;
 
     if (count > 0) {
+
       countdownText.innerText = count;
+
     } else {
 
       clearInterval(timer);
 
       countdownText.style.display = "none";
 
-      // hiện ảnh scary
       scaryImage.style.display = "block";
 
-      // phát âm thanh
       scarySound.play();
     }
 
   }, 1000);
 }
-
